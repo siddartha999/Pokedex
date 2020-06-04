6878b9c23ed07905bf57b322c799590a8c0126cd
Siddartha <siddarthatummala2@gmail.com>
2020-06-04 10:34:38 +0530

    Introduced InvertedBarChart component and incorporated that with PokeCardDialog component

---
+++ src/InvertedBarChart/InvertedBarChart.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./InvertedBarChart.css";
+
+const styleObj = {
+  background: "#8360c3  linear-gradient(to right, #2ebf91, #8360c3)",
+  marginTop: "1rem",
+  width: "",
+  height: "1.5rem",
+  textAlign: "center",
+  color: "cornsilk",
+};
+
+const findMaxNumber = (data) => {
+  let val = Number.MIN_SAFE_INTEGER;
+  for (let point of data) {
+    if (val < point.value) {
+      val = point.value;
+    }
+  }
+  return val;
+};
+
+const InvertedBarChart = (props) => {
+  let max_num = props.max_num || Number.MIN_SAFE_INTEGER;
+
+  if (!props.max_num) {
+    max_num = findMaxNumber(props.data);
+  }
+
+  const barChartJSX = props.data.map((dataPoint) => {
+    const name = dataPoint.name;
+    const value = dataPoint.value;
+    const width = value / max_num;
+    const newStyleObj = { ...styleObj };
+    newStyleObj.width = width * 100 + "%";
+    return (
+      <div style={newStyleObj} title={name + " : " + value}>
+        {name}
+      </div>
+    );
+  });
+
+  return <>{barChartJSX}</>;
+};
+
+export default InvertedBarChart;
--- src/PokeCardDialog/PokeCardDialog.js
+++ src/PokeCardDialog/PokeCardDialog.js
@@ -3,22 +3,28 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItem from "@material-ui/core/ListItem";
-import List from "@material-ui/core/List";
 import "./PokeCardDialog.css";
+import InvertedBarChart from "../InvertedBarChart/InvertedBarChart";
 
 const PokeCardDialog = (props) => {
+  const chartInput = [];
+
   const handleClose = () => {
     props.closeDialog();
   };
 
   const listItems = [];
+
   for (let dataPoint of props.data.stats) {
+    const name = dataPoint.stat.name;
+    const value = dataPoint.base_stat;
+    chartInput.push({
+      name: name,
+      value: value,
+    });
     listItems.push(
-      <ListItem key={dataPoint.stat.name}>
-        <ListItemText
-          primary={dataPoint.stat.name}
-          secondary={dataPoint.base_stat}
-        />
+      <ListItem key={name}>
+        <ListItemText primary={name} secondary={value} />
       </ListItem>
     );
   }
@@ -30,9 +36,16 @@ const PokeCardDialog = (props) => {
         aria-labelledby="simple-dialog-title"
         open={props.open}
       >
-        <DialogTitle id="simple-dialog-title">{props.data.name}</DialogTitle>
+        <DialogTitle
+          id="simple-dialog-title"
+          className="PokeCardDialog-dialog-title-container"
+        >
+          {props.data.name}
+        </DialogTitle>
         <div className="PokeCardDialog-img-container"> {props.imgJSX}</div>
-        <List>{listItems}</List>
+        <div className="PokeCardDialog-inverted-chart-container">
+          <InvertedBarChart data={chartInput} />
+        </div>
       </Dialog>
     </div>
   );
